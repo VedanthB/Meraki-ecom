da50bc2c927fca2cb9d65791b7c48286b3335050
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-08 13:43:59 +0530

    [FEAT]: created next js app and cleanup

---
+++ meraki/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Meraki</title>
+        <meta name="description" content="An ecommerce application" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <h1>Meraki</h1>
+    </div>
+  );
+}
