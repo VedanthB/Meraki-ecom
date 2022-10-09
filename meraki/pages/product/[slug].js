7db25b4943f89bb14d3586ce0341831907703175
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-09 21:08:59 +0530

    [FEAT]: add to cart functionality in product page

--- meraki/pages/product/[slug].js
+++ meraki/pages/product/[slug].js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { Layout, NotFound, ProductDetailsPage } from "../../components";
-import { data } from "../../utils";
+import { db } from "../../utils";
+import { Product } from "../../models";
 
-export default function ProductScreen() {
+export default function ProductScreen({ products }) {
   const router = useRouter();
+
   const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
+
+  const product = products?.find((a) => a.slug === slug);
+
   if (!product) {
     return <NotFound />;
   }
@@ -15,4 +19,18 @@ export default function ProductScreen() {
       <ProductDetailsPage product={product} />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+
+  const products = await Product.find({}).lean();
+
+  await db.disconnect();
+
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
