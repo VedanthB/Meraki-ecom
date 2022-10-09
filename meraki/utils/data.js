737a87cf4c1128c63128a14c50d9f871267712a5
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-10 02:46:21 +0530

    [FEAT]: seeded sample users

--- meraki/utils/data.js
+++ meraki/utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "John",
+      email: "admin@gmail.com",
+      password: bcrypt.hashSync("password"),
+      isAdmin: true,
+    },
+    {
+      name: "Jane",
+      email: "jane@gmail.com",
+      password: bcrypt.hashSync("password"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: "Draped Blouse",
