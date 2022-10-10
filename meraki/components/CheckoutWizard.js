b1db8346a7c7d7c557911dcf93eb992c548aedc2
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-11 01:29:17 +0530

    [FIX]: shipping page fix

--- meraki/components/CheckoutWizard.js
+++ meraki/components/CheckoutWizard.js
@@ -1,26 +1,16 @@
 import { Step, StepLabel, Stepper } from "@material-ui/core";
 import React from "react";
-import { useStore } from "../context";
 import { useStyles } from "../utils";
 
 export default function CheckoutWizard({ activeStep = 0 }) {
   const classes = useStyles();
-  const { state } = useStore();
-  const { darkMode } = state;
-  return darkMode ? (
+
+  return (
     <Stepper
       activeStep={activeStep}
       alternativeLabel
-      className={classes.dark_checkout_wizard}
+      className={classes.transparent_backgroud}
     >
-      {["Shipping Address", "Payment Method", "Place Order"].map((step) => (
-        <Step key={step}>
-          <StepLabel>{step}</StepLabel>
-        </Step>
-      ))}
-    </Stepper>
-  ) : (
-    <Stepper activeStep={activeStep} alternativeLabel>
       {["Shipping Address", "Payment Method", "Place Order"].map((step) => (
         <Step key={step}>
           <StepLabel>{step}</StepLabel>
--- meraki/pages/shipping.js
+++ meraki/pages/shipping.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useStore } from "../context";
 import { Layout, ShippingPage } from "../components";
@@ -7,9 +7,12 @@ export default function Shipping() {
   const router = useRouter();
   const { state } = useStore();
   const { userInfo } = state;
-  if (!userInfo) {
-    router.push("/login?redirect=/shipping");
-  }
+
+  useEffect(() => {
+    if (!userInfo) {
+      router.push("/login?redirect=/shipping");
+    }
+  }, []);
 
   return (
     <Layout>
