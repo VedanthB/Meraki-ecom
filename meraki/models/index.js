137e435cc4c84e2323f1272ed0af11ea1cd6dbf4
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-11 03:24:58 +0530

    [FEAT]: created order model

--- meraki/models/index.js
+++ meraki/models/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import Product from "./Product";
 import User from "./User";
+import Order from "./Order";
 
-export { Product, User };
+export { Product, User, Order };
