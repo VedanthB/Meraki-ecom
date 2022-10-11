c3f699094f34287917080d04752dc17a4036b641
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-11 19:33:54 +0530

    [FEAT]: order history table

---
+++ meraki/components/order-history/OrderHistoryTable.js
@@ -0,0 +1,130 @@
+/* eslint-disable no-nested-ternary */
+import React from "react";
+import NexLink from "next/link";
+import {
+  CircularProgress,
+  List,
+  ListItem,
+  TableContainer,
+  Typography,
+  Card,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from "@material-ui/core";
+import { useStyles } from "../../utils";
+
+function OrderHistoryTable({ loading, error, orders }) {
+  const classes = useStyles();
+  return (
+    <Card className={classes.order_history_cards}>
+      <List>
+        <ListItem>
+          <Typography component="h2" variant="h2">
+            Order History
+          </Typography>
+        </ListItem>
+        <ListItem>
+          {loading ? (
+            <CircularProgress />
+          ) : error ? (
+            <Typography className={classes.error}>{error}</Typography>
+          ) : (
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      align="center"
+                      className={classes.table_headings}
+                    >
+                      ID
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_headings}
+                    >
+                      DATE
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_headings}
+                    >
+                      TOTAL
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_headings}
+                    >
+                      PAID
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      className={classes.table_headings}
+                    >
+                      DELIVERED
+                    </TableCell>
+                    <TableCell />
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {orders.map((order) => (
+                    <TableRow key={order._id}>
+                      <TableCell
+                        align="center"
+                        className={classes.table_headings}
+                      >
+                        {order._id.substring(20, 24)}
+                      </TableCell>
+                      <TableCell
+                        align="center"
+                        className={classes.table_headings}
+                      >
+                        {order.createdAt.substring(0, 10)}
+                      </TableCell>
+                      <TableCell
+                        align="center"
+                        className={classes.table_headings}
+                      >
+                        Rs.{order.totalPrice}
+                      </TableCell>
+                      <TableCell
+                        align="center"
+                        className={classes.table_headings}
+                      >
+                        {order.isPaid ? `paid at ${order.paidAt}` : "not paid"}
+                      </TableCell>
+                      <TableCell
+                        align="center"
+                        className={classes.table_headings}
+                      >
+                        {order.isDelivered
+                          ? `delivered at ${order.deliveredAt}`
+                          : "not delivered"}
+                      </TableCell>
+                      <TableCell align="center">
+                        <NexLink href={`/order/${order._id}`} passHref>
+                          <Button
+                            variant="solid"
+                            className={classes.btn_primary}
+                          >
+                            Details
+                          </Button>
+                        </NexLink>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
+
+export default OrderHistoryTable;
