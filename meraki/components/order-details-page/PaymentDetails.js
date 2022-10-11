42b8e768572006db4405974f3e001c4cdca9cc44
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-11 06:17:00 +0530

    [FEAT]: payment details component

---
+++ meraki/components/order-details-page/PaymentDetails.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card, List, ListItem, Typography } from "@material-ui/core";
+import { useStyles } from "../../utils";
+
+function PaymentDetails({ paymentMethod, isPaid, paidAt }) {
+  const classes = useStyles();
+  return (
+    <Card className={classes.order_detail_cards}>
+      <List>
+        <ListItem>
+          <Typography className={classes.order_details_heading}>
+            Payment Method: {paymentMethod}
+          </Typography>
+        </ListItem>
+
+        <ListItem>Status: {isPaid ? `paid at ${paidAt}` : "not paid"}</ListItem>
+      </List>
+    </Card>
+  );
+}
+
+export default PaymentDetails;
