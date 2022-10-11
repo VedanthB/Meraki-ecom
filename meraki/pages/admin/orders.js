dc028f98c66196859bb4fa81f2bb132900fb8744
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-10-11 23:38:56 +0530

    [FEAT]: admin orders page

---
+++ meraki/pages/admin/orders.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-nested-ternary */
+import axios from "axios";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+import React, { useEffect, useReducer } from "react";
+import {
+  CircularProgress,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+  Card,
+  ListItemText,
+} from "@material-ui/core";
+import { useStore } from "../../context";
+import { getError, useStyles } from "../../utils";
+import { Layout, NoOrders, OrdersTable } from "../../components";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, orders: action.payload, error: "" };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
+
+function AdminDashboard() {
+  const { state } = useStore();
+  const router = useRouter();
+  const classes = useStyles();
+  const { userInfo } = state;
+
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    orders: [],
+    error: "",
+  });
+
+  useEffect(() => {
+    if (!userInfo) {
+      router.push("/login");
+    }
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`/api/admin/orders`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Layout title="Orders">
+      <Grid container spacing={5}>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <NextLink href="/admin/dashboard" passHref>
+                <ListItem button component="a">
+                  <ListItemText primary="Admin Dashboard" />
+                </ListItem>
+              </NextLink>
+              <NextLink href="/admin/orders" passHref>
+                <ListItem selected button component="a">
+                  <ListItemText primary="Orders" />
+                </ListItem>
+              </NextLink>
+            </List>
+          </Card>
+        </Grid>
+        <Grid item md={9} xs={12}>
+          {orders.length === 0 ? (
+            <NoOrders />
+          ) : (
+            <Card className={classes.admin_orders_table}>
+              <List>
+                <ListItem>
+                  <Typography className={classes.admin_orders_heading}>
+                    Orders
+                  </Typography>
+                </ListItem>
+
+                <ListItem>
+                  {loading ? (
+                    <CircularProgress />
+                  ) : error ? (
+                    <Typography className={classes.error}>{error}</Typography>
+                  ) : (
+                    <OrdersTable orders={orders} />
+                  )}
+                </ListItem>
+              </List>
+            </Card>
+          )}
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export default dynamic(() => Promise.resolve(AdminDashboard), { ssr: false });
